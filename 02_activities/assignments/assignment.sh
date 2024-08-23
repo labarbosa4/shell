4fabbcb192116a5681fb1e926e599819fd0255ff
Laura Barbosa <lauram.barbosac@hotmail.com>
2024-08-23 18:31:16 -0400

    Update 4 assignment.sh

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -18,20 +18,19 @@ cd dir2
 touch file1.txt file2.txt file3.txt file4.txt file5.txt
 
 # 4. Append the words "Hello world" to dir2/file3.txt
-cd dir2
 echo "Hello world" >> file3.txt
 
 # 5. Verify that file3.txt contains the words "hello world" by printing the contents of the file in the terminal
 cat file3.txt
 
 # 6. Delete file4.txt
-cd dir2
 rm file4.txt
 
 # 7. Delete directories dir4 and dir5 including all their contents (if any)
-cd dir2
+cd
 rm -r dir4
 rm -r dir5
 
 # 8. List the contents of the current working directory to verify the deletion of dir4 and dir5
+cd
 ls
